454b2610ca0f069ed85b11f0114a68947c4b5bc1
Chris Suy <you@example.com>
2024-01-03 14:07:30 -0500

    Attempting to finally generate a simple grid

---
+++ CGoLScript.js
@@ -0,0 +1,8 @@
+const grid_Container = document.getElementById("grid_container")
+
+for (let i = 1; i < 9; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("grid-item");
+    gridItem.textContent = i;
+    grid_Container.appendChild(gridItem)
+}
