cb71af640c15cb2e071e98cc612d5eb10ff5d76e
Just-Whiz <christiansuy25@gmail.com>
2024-01-03 20:58:19 -0500

    Setting up basic structure for further development later

--- CGoLScript.js
+++ CGoLScript.js
@@ -1,21 +1,6 @@
-g = document.getElementById("grid_container")
-w = document.getElementById("grid_container")
-h = document.getElementById("grid_container")
+const canvas = document.querySelector("canvas")
+const ctx = canvas.getContext9("2d");
 
-
-
-
-
-
-
-
-
-
-const grid_Container = document.getElementById("grid_container")
-
-for (let i = 1; i < 9; i++) {
-    const gridItem = document.createElement("div");
-    gridItem.classList.add("grid-item");
-    gridItem.textContent = i;
-    grid_Container.appendChild(gridItem);
-}
+const resolution = 40;
+canvas.width = 400;
+canvas.height = 400;
